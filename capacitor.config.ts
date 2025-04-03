53ce7659f4f2bc1a63b8bd6785fb010249b2e41a
MatheusDeZottis <matheusdezottis@gmail.com>
2025-04-02 23:54:10 -0300

    feat: inicializa projeto Angular com Ionic e Capacitor, incluindo configuração para Android e iOS

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'matheus.dev.com',
+  appName: 'projeto-logim',
+  webDir: 'www'
+};
+
+export default config;
